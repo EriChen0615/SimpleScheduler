8c4c2f53843f05fa464a96240d647de3e5785ffe
EriChen <erichen0615@gmail.com>
2018-12-11 23:12:01 +0000

    Finish link_list for future development

---
+++ link_list.h
@@ -0,0 +1,167 @@
+#ifndef LINK_LIST_H
+#define LINK_LIST_H
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::string;
+
+template <class T>
+class Node
+{
+	private:
+		T value;
+		Node<T>* next;
+	
+	public:
+		Node(void);
+		Node(T value,Node<T>* next);
+		T GetValue(void);
+		Node* GetNext(void);
+		void SetValue(T value);
+		void SetNext(Node<T>* next);
+		void operator=(Node<T>* node);
+};
+
+template<typename T>
+Node<T>::Node()
+{
+	this->next = NULL;
+}
+
+template<typename T>
+Node<T>::Node(T value,Node* next)
+{
+	this->value = value;
+	this->next = next;
+}
+
+
+template<typename T>
+T Node<T>::GetValue(void)
+{
+	return value;
+}
+
+template<typename T>
+Node<T>* Node<T>::GetNext(void)
+{
+	return next;
+}
+
+template<typename T>
+void Node<T>::SetValue(T value)
+{
+	this->value = value;
+}
+
+template<typename T>
+void Node<T>::SetNext(Node<T>* next)
+{
+	this->next = next;
+}
+
+template<typename T>
+void Node<T>::operator=(Node<T>* node)
+{
+	this->next = node->next;
+	this->value = node->value; 
+}
+
+template<typename T>
+class Link_list
+{
+	private:
+		Node<T>* head;
+		Node<T>* tail;
+		int length;
+		Node<T>* _GetNode(int pos)
+		{
+			if(pos>=length) return NULL;
+			Node<T>* tmp = head;
+			for(int i=0;i<pos;i++)
+			{
+				tmp = tmp->GetNext();
+			 } 
+			return tmp;
+		}
+		
+	public:
+		Link_list(void)
+		{
+			head = NULL;
+			tail = NULL;
+			length = 0;
+		}
+		Link_list(Node<T>* head)
+		{
+			this->head = head;
+			this->tail = head;
+			length = 1;
+		}
+		void Insert(int pos,Node<T>* node)
+		{
+			if(pos==0)
+			{
+				node->SetNext(head);
+				head = node;
+				length++;
+			}
+			else if(pos<=length)
+			{
+				Node<T>* before = _GetNode(pos-1);
+				Node<T>* current = before->GetNext();
+				before->SetNext(node);
+				node->SetNext(current); 
+				if(pos==length) tail = node;
+				length++;
+			}
+			else
+			{
+				cout << "Out of bound" << endl;
+			}
+		}
+		void Replace(int pos,Node<T>* node)
+		{
+			if(pos==0)
+			{
+				node->SetNext(head->GetNext());
+				head = node;
+			}
+			else
+			{
+				Node<T>* before = _GetNode(pos-1);
+				Node<T>* current = _GetNode(pos);
+				before->SetNext(node);
+				node->SetNext(current->GetNext());
+			}
+		}
+		void Delete(int pos)
+		{
+			Node<T>* before = _GetNode(pos-1);
+			Node<T>* current = _GetNode(pos);
+			before->SetNext(current->GetNext());
+			length--;
+		}
+		void Append(Node<T>* node)
+		{
+			tail->SetNext(node);
+			tail = node;
+			length++;
+		}
+		void Show(void)
+		{
+			Node<T>* current = head;
+			while(current!=NULL)
+			{
+				cout << current->GetValue() << " ---> ";
+				current = current->GetNext();
+			}
+			cout << endl;
+		}
+};
+
+
+
+#endif
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <ctime> 
 #include "info_unit.h"
 #include "row.h"
 #include "table.h"
+#include "link_list.h"
+
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -29,6 +32,7 @@ int main(int argc, char** argv) {
 ////	b.SetHeight(6);
 ////	b.SetWidth(10);
 //	a.Show();
+
 //	string content[5] = {"A","B","C","D","E"};
 //	int width[5] = {10,10,10,10,10};
 //	Row row = Row(5,5,width,content);
@@ -42,26 +46,57 @@ int main(int argc, char** argv) {
 //	string hi = "Hi!";
 //	row.Modify(2,"Eric");
 //	row.Show();
-	string x_ticks[5] = {"1","2","3","4","5"};
-	string y_ticks[2] = {"a","b"}; 
-	Table table = Table(2,5,x_ticks,y_ticks);
-	table.Show();
-	table.SetRowHeight(0,2);
-	table.Show();
-	table.Insert(2,1,new Info_unit(5,5,"Hi!"));
-	table.Show();
-	table.Delete(2,1);
-	table.Show();
-	table.Replace(2,0,new Info_unit(5,5,"Hell"));
-	table.Show();
-	string content[5] = {"Q","W","E","R","T"};
-	int width[5] = {10,10,10,10,10};
-	Row *row = new Row(5,5,width,content);
-	table.InsertRow(3,row);
-	table.Show();
-	table.DeleteRow(2);
-	table.Show();
-	table.ReplaceRow(1,new Row(5,5,width,content));
-	table.Show();
+
+//	string x_ticks[5] = {"1","2","3","4","5"};
+//	string y_ticks[2] = {"a","b"}; 
+//	Table table = Table(2,5,x_ticks,y_ticks);
+//	table.Show();
+//	table.SetRowHeight(0,2);
+//	table.Show();
+//	table.Insert(2,1,new Info_unit(5,5,"Hi!"));
+//	table.Show();
+//	table.Delete(2,1);
+//	table.Show();
+//	table.Replace(2,0,new Info_unit(5,5,"Hell"));
+//	table.Show();
+//	string content[5] = {"Q","W","E","R","T"};
+//	int width[5] = {10,10,10,10,10};
+//	Row *row = new Row(5,5,width,content);
+//	table.InsertRow(3,row);
+//	table.Show();
+//	table.DeleteRow(2);
+//	table.Show();
+//	table.ReplaceRow(1,new Row(5,5,width,content));
+//	table.Show();
+
+//	string s1 = "Hello";
+//	string s3 = "World";
+//	Node<string> node1 = Node<string>();
+//	node1.SetValue(s1);
+//	string s2 = node1.GetValue();
+//	cout << s2 << endl;
+//	Node<string> node2 = node1;
+//	cout << node2.GetValue() << endl;
+//
+//	Link_list<string> l1 = Link_list<string>(&node1); 
+//	l1.Show();
+//	l1.Insert(1,&node2);
+//	l1.Show();
+//	Node<string> node3 = Node<string>(s3,NULL);
+//	Node<string> node4 = node3;
+//	l1.Append(&node3);
+//	l1.Show();
+//	l1.Delete(1);
+//	l1.Show();
+//	l1.Replace(0,&node4);
+//	l1.Show();
+
+	time_t now = time(0);
+	char* dt = ctime(&now);
+	cout << "The local date and time is: " << dt;
+	tm* ltm = localtime(&now);
+	cout << 1900+ltm->tm_year << endl;
+	cout << 1+ltm->tm_mon << endl;
+	
 	return 0;
 }
